47be37ae981ce0f8ed51d1f53d5eeb92f78ea5a8
Jorge Zozaya <jorge.zozaya.verdiguel@gmail.com>
2020-02-12 13:58:48 -0800

    Updates

---
+++ precache-manifest.3b2d604512c4a2ad3475a50577b683c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7bd6d44c6e5f14c5c0eda767e7464f",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "c17c0f4ba21c9ac20a11",
+    "url": "/production/static/css/main.d8948653.chunk.css"
+  },
+  {
+    "revision": "f999fd41c2a012874bf6",
+    "url": "/production/static/js/2.5e844947.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/production/static/js/2.5e844947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17c0f4ba21c9ac20a11",
+    "url": "/production/static/js/main.1a49be66.chunk.js"
+  },
+  {
+    "revision": "4a81dc816ee1e4521427",
+    "url": "/production/static/js/runtime-main.0b271288.js"
+  }
+]);
